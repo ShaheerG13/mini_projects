3c4ea8ed436ca9a0ea4be78c7d55ec93d1ff94f8
ShaheerG13 <shaheerghazenfer13@gmail.com>
2026-04-05 17:09:20 -0400

    screen break reminder script created

---
+++ screen_break_reminder.py
@@ -0,0 +1,17 @@
+import time
+from winotify import Notification
+
+# change this to how often you want to take a break
+mins = 20
+secs = mins * 60
+
+toast = Notification(
+  app_id="Reminder",
+  title="Eye Break!",
+  msg="Take a 20 second break for your eyes",
+  duration="short"
+)
+
+while True:
+  toast.show()  
+  time.sleep(secs)
